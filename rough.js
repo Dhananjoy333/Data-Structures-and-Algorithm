f8578cbaa1784b41d178677acfeb164d1dcc3d6e
Dhananjoy <dhananjoybrahma333@gmail.com>
2026-09-12 17:42:50 +0530

    added dp intro

--- rough.js
+++ rough.js
@@ -1,22 +1,9 @@
-var mapWordWeights = function(words, weights) {
-    let weightMap = {}
-    let alphabetMap = {}
-    let  i = 0;
-    let res = ''
-    for(let ch = 'a'.charCodeAt(0); ch <= 'z'.charCodeAt(0); ch++){
-        weightMap[String.fromCharCode(ch)] = weights[i]
-        alphabetMap[25-i] = String.fromCharCode(ch)
-        i++
+function fibonacci(n){
+    if(n === 0){
+        return 0
+    }else if (n === 1){
+        return 1
     }
-    for(let word of words){
-        let total = 0
-        for(let ch = 0; ch < word.length; ch++){
-            total += weightMap[word[ch]]
-        }
-        let modulo = total % 26
-        res += alphabetMap[String(modulo)]
-    }
-    return res
-};
-
-console.log(mapWordWeights(["a","b","c"],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]))
+    return(fibonacci(n-1)+fibonacci(n-2))
+}
+console.log(fibonacci(4))
